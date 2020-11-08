a24174d7c94fef818978a5e4a95911663130b9cb
Raphael <duchainer@gmail.com>
2020-11-08 15:46:34 -0500

    WIP: Correct deploy-ros.sh to use sara_install repo
    Still needs some testing by Nicolas Handfield

--- deploy-ros.sh
+++ deploy-ros.sh
@@ -1,69 +1,36 @@
-#!/bin/bash
-#
-# This install automatically ROS (kinetic) and workspace for work on SARA
-#
-# @author:        Lucas Maurice
-# @organisation:  Walking Machine
-# @date:          14/06/2018
 
-# Prepare ROS installation
+# Ros Kinetic for Ubuntu
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt update
 
-# Install ROS and dependencies
-sudo apt install -y ros-kinetic-desktop-full
-sudo apt install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install -y ros-kinetic-ros-control ros-kinetic-hardware-interface ros-kinetic-moveit ros-kinetic-navigation
-sudo apt install -y ffmpeg
-sudo apt install -y python-imaging python-imaging-tk python-pip
-pip install --upgrade pip
-pip install Pillow
-python -m pip install --user --upgrade pip
-python -m pip install Pillow
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-# Initialise ROS
+sudo apt-get update
+
+sudo apt-get install ros-kinetic-desktop-full
+
+#Add to bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#Add to zshrc
+echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+sudo apt install python-rosdep
+
 sudo rosdep init
 rosdep update
 
-# Prepare workspace
-source /opt/ros/kinetic/setup.bash
-mkdir ~/sara_ws/src/ -p
-cd ~/sara_ws/src
+# Sara install first installation
+# TODO: Make sure that sara_install is installed in ~/
+git clone git@github.com:WalkingMachine/sara_install.git -b config/simulation_kinetic -- ~
 
-# Get wm main repositories
-git clone git@github.com:WalkingMachine/sara_msgs.git
-git clone git@github.com:WalkingMachine/sara_launch.git
-git clone git@github.com:WalkingMachine/sara_behaviors.git
-git clone git@github.com:WalkingMachine/wonderland.git
-git clone git@github.com:WalkingMachine/wm_object_detection.git
-git clone git@github.com:team-vigir/flexbe_behavior_engine.git
+source ~/sara_install/script/setup.sh
 
-# Install Wonderland
-cd ~/sara_ws/src/wonderland
-./install.sh
+# TODO: If this doesn't exist as an alias replace it by the alias definition in ~/sara_install/script/setup.sh
+INSTALL_SARA
 
-# Install Behavior
-cd ~/sara_ws/src/sara_behaviors
-./install.sh
+source ~/sara_install/script/setup.sh
 
-# Build workspace
-cd ~/sara_ws
-catkin_make
-
-# Source workspace
-source ~/sara_ws/src/sara_launch/sh_files/sararc.sh
-
-# Write sources in bashrc
-if !(grep --quiet "# FOR ROS" ~/.bashrc); then
-    echo Deploy Source to .bashrc
-    echo "# FOR ROS" >> ~/.bashrc
-    echo "source ~/sara_ws/src/sara_launch/sh_files/sararc.sh" >> ~/.bashrc
-fi
-
-# Write sources in zshrc
-if !(grep --quiet "# FOR ROS" ~/.zshrc); then
-    echo Deploy Source to .zshrc
-    echo "# FOR ROS" >> ~/.zshrc
-    echo "source ~/sara_ws/src/sara_launch/sh_files/sararc.sh" >> ~/.zshrc
-fi
